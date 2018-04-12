98ff4e252d8a08e513b86c0ff82bf9f5294022e6
Serzh <35348098+LoreLineage@users.noreply.github.com>
2018-04-12 13:51:55 +0300

    Add files via upload

---
+++ Translate/other/mercenaryArrange.h
@@ -0,0 +1,6 @@
+<body>
+<var src="#0">„K„p„{ „„‚„€„t„r„y„s„p„„„„ƒ„‘ „r„p„Š„y „t„u„|„p?<br><br>
+
+„K„€„|„y„‰„u„ƒ„„„r„€ „~„p„~„‘„„„„‡ „~„p„v„}„~„y„{„€„rF<var src="#1"><br>
+„K„€„|„y„‰„u„ƒ„„„r„€ „r„€„x„}„€„w„~„„‡ „~„p„v„}„~„y„{„€„rF<var src="#2"><br>
+<br>
